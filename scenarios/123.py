01ac4b901877f588b402813c6551265d56fb0230
thinhprovc202@gmail.com <thinhprovc202@gmail.com>
2024-10-09 03:18:33 +0000

    Saved scenario QS_MLOPS_2.123 (id: 123)

--- scenarios/123.py
+++ scenarios/123.py
@@ -7,16 +7,15 @@ project_keys = client.list_projects()
 # Lấy key của từng project
 z = [project['projectKey'] for project in project_keys]
 # Handling our scenario.
-for i in range(2):
-    s = Scenario()
-    # Khởi tạo bước xây dựng
-    step = BuildFlowItemsStepDefHelper("Data Preparation")
-    step.add_dataset("earnings_by_education", "QS_MLOPS_2")
-    step.add_dataset("job_postings_python", "QS_MLOPS_2")
-    step.add_dataset("job_postings_prepared_joined", "QS_MLOPS_2")
+s = Scenario()
+# Khởi tạo bước xây dựng
+step = BuildFlowItemsStepDefHelper("Data Preparation")
+step.add_dataset("earnings_by_education", "QS_MLOPS_2")
+step.add_dataset("job_postings_python", "QS_MLOPS_2")
+step.add_dataset("job_postings_prepared_joined", "QS_MLOPS_2")
 
-    # Chạy bước này
-    try:
-        s.run_step(step.get_step())
-    except Exception as e:
-        print(f"Step {i+1} gặp lỗi: {str(e)}")
+# Chạy bước này
+try:
+    s.run_step(step.get_step())
+except Exception as e:
+    print(f"Step {i+1} gặp lỗi: {str(e)}")
