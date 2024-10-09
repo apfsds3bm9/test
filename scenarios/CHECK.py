09e6c6ec075d096880c2101bdd6705ad22bce051
thinhprovc202@gmail.com <thinhprovc202@gmail.com>
2024-10-09 03:14:11 +0000

    Saved scenario QS_MLOPS_2.Check (id: CHECK)

--- scenarios/CHECK.py
+++ scenarios/CHECK.py
@@ -1,24 +1,18 @@
-# This sample code helps you get started with the custom scenario API.
-#For more details and samples, please see our Documentation
-from dataiku.scenario import Scenario
+from dataiku.scenario import Scenario, BuildFlowItemsStepDefHelper
+import dataiku
 
-# The Scenario object is the main handle from which you initiate steps
-scenario = Scenario()
+# Lấy danh sách tất cả các project
+client = dataiku.api_client()
+project_keys = client.list_projects()
+# Lấy key của từng project
+z = [project['projectKey'] for project in project_keys]
+# Handling our scenario.
+for i in range(2):
+    s = Scenario()
+    # Khởi tạo bước xây dựng
+    step = BuildFlowItemsStepDefHelper("Data Preparation")
+    step.add_dataset("earnings_by_education", "QS_MLOPS_2")
+    step.add_dataset("job_postings_python", "QS_MLOPS_2")
 
-# A few example steps follow
-
-# Building a dataset
-scenario.build_dataset("customers_prepared", partitions="2015-01-03")
-
-# Controlling the train of a dataset
-train_ret = scenario.train_model("uSEkldfsm")
-trained_model = train_ret.get_trained_model()
-performance = trained_model.get_new_version_metrics().get_performance_values()
-if performance["AUC"] > 0.85:
-    trained_model.activate_new_version()
-
-# Sending custom reports
-sender = scenario.get_message_sender("mail-scenario", "local-mail") # A messaging channel
-sender.set_params(sender="dss@company.com", recipient="data-scientists@company.com")
-
-sender.send(subject="The scenario is doing well", message="All is good")
+    # Chạy bước này
+    s.run_step(step.get_step())
