9b170c86e3f6b59b645018cc5d1b9e5f1b7c8105
thinhprovc202@gmail.com <thinhprovc202@gmail.com>
2024-10-10 16:26:02 +0000

    Saved scenario TASKCHUAVAI.Test (id: TEST)

--- scenarios/TEST.py
+++ scenarios/TEST.py
@@ -10,7 +10,17 @@ scenario = Scenario()
 # Building a dataset
 list_partitions = ["2009", "2010"]
 for z in list_partitions:
-    scenario.build_dataset(f"customers_prepared_{z}", partitions=z)
+    s = Scenario()
+    step = BuildFlowItemsStepDefHelper(f"Data Preparation {z}")
+    step.add_dataset("earnings_by_education", "QS_MLOPS_2")  # Thay "QS_MLOPS_2" bằng z[0]
+    step.add_dataset("job_postings_python", "QS_MLOPS_2")  # Thay "QS_MLOPS_2" bằng z[0]
+    step.add_dataset("job_postings_prepared_joined", "QS_MLOPS_2") 
+    step.add_dataset("unmatched", "QS_MLOPS_2") #unmatched
+    # Chạy step với build_mode là RECURSIVE_FORCED_BUILD
+    #s.build_dataset("earnings_by_education", "QS_MLOPS_2", build_mode="RECURSIVE_FORCED_BUILD")
+    s.build_dataset("job_postings_prepared", "QS_MLOPS_2", build_mode="RECURSIVE_FORCED_BUILD")
+    #s.build_dataset("job_postings_prepared_joined", "QS_MLOPS_2", build_mode="RECURSIVE_FORCED_BUILD")
+    #s.build_dataset("unmatched", "QS_MLOPS_2", build_mode="RECURSIVE_FORCED_BUILD")
     
 
 
