d7b85f3f9ca82d36ffd3c2b51ff2b70660cb0f77
thinhprovc202@gmail.com <thinhprovc202@gmail.com>
2024-10-11 03:52:25 +0000

    Saved recipe: TASKCHUAVAI.compute_Test_Python

--- recipes/compute_Test_Python.py
+++ recipes/compute_Test_Python.py
@@ -4,19 +4,32 @@ import dataiku
 mydataset = dataiku.Dataset("norway_new_car_sales_by_make_filtered_2")
 myoutputdataset = dataiku.Dataset("Test_Python")
 
+#df = mydataset.get_dataframe()
+
+# Lấy danh sách các giá trị duy nhất của cột 'year'
+#unique_years = df['Year'].unique()
+
+# Lưu dữ liệu theo partition từng năm
+#with myoutputdataset.get_writer() as writer:
+#    for year in unique_years:
+        # Lọc dữ liệu của từng năm
+#        partition_df = df[df['Year'] == year]
+        
+        # Thiết lập partition theo năm
+#        myoutputdataset.set_write_partition(str(year))
+        
+        # Ghi dữ liệu của partition đó
+#        writer.write_dataframe(partition_df)
 df = mydataset.get_dataframe()
 
 # Lấy danh sách các giá trị duy nhất của cột 'year'
-unique_years = df['Year'].unique()
+unique_years = df['year'].unique()
 
 # Lưu dữ liệu theo partition từng năm
 with myoutputdataset.get_writer() as writer:
     for year in unique_years:
         # Lọc dữ liệu của từng năm
-        partition_df = df[df['Year'] == year]
+        partition_df = df[df['year'] == year]
         
-        # Thiết lập partition theo năm
-        myoutputdataset.set_write_partition(str(year))
-        
-        # Ghi dữ liệu của partition đó
+        # Không cần gọi set_write_partition, chỉ cần ghi dataframe
         writer.write_dataframe(partition_df)
