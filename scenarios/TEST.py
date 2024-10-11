eba701fc943f7dabf1301071fedaf6709092cf87
thinhprovc202@gmail.com <thinhprovc202@gmail.com>
2024-10-11 16:38:17 +0000

    Saved scenario TASKCHUAVAI.Test (id: TEST)

--- scenarios/TEST.py
+++ scenarios/TEST.py
@@ -1,10 +1,6 @@
-# This sample code helps you get started with the custom scenario API.
-#For more details and samples, please see our Documentation
-from dataiku.scenario import Scenario, BuildFlowItemsStepDefHelper
 import dataiku
-
-
-import dataiku
+from dataiku.scenario import Scenario
+from dataiku.scenario.steps import BuildFlowItemsStepDefHelper
 
 # Lấy dataset chứa thông tin `Make`
 mydataset = dataiku.Dataset("norway_new_car_sales_by_make_filtered_2")
@@ -17,28 +13,17 @@ unique_makes = df['Make'].unique()
 scenario_variables = {"makes_list": ",".join(unique_makes)}
 dataiku.set_scenario_variables(scenario_variables)
 
-
 # The Scenario object is the main handle from which you initiate steps
 scenario = Scenario()
 
-# A few example steps follow
-client = dataiku.api_client()
-project_keys = client.list_projects()
-
-# Lấy key của từng project
-z = [project['projectKey'] for project in project_keys]
-# Building a dataset
-list_partitions = ["Toyota", "Volvo"]# ,2009, 2010]
-for z in list_partitions:
-    s = Scenario()
-    step = BuildFlowItemsStepDefHelper(f"Data Preparation {z}")
-    step.add_dataset("norway_new_car_sales_by_make", "TASKCHUAVAI")  # Thay "QS_MLOPS_2" bằng z[0]
-    step.add_dataset("Test_Python", "TASKCHUAVAI")  # Thay "QS_MLOPS_2" bằng z[0]
-    # Chạy step với build_mode là RECURSIVE_FORCED_BUILD
-    s.build_dataset("Test_Python", 
-                    "TASKCHUAVAI", 
-                    build_mode="RECURSIVE_BUILD",
-                    partitions = z)
+# Building a dataset - Lặp qua danh sách các `Make`
+for make in unique_makes:
+    # Khởi tạo một step mới cho từng `Make`
+    step = BuildFlowItemsStepDefHelper(f"Data Preparation for {make}")
     
-
-
+    # Add các dataset đầu vào và đầu ra
+    step.add_dataset("norway_new_car_sales_by_make_filtered_2", partition=make)  # Thay partition bằng make
+    step.add_dataset("Test_Python", partition=make)  # Thêm partition vào dataset đầu ra
+    
+    # Chạy step với build_mode là RECURSIVE_FORCED_BUILD cho từng partition
+    scenario.build_dataset("Test_Python", partition=make, build_mode="RECURSIVE_FORCED_BUILD")
