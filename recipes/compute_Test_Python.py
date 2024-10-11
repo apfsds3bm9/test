1a4fa8425044e00e492971026b3fcd2a213e7706
thinhprovc202@gmail.com <thinhprovc202@gmail.com>
2024-10-11 16:36:02 +0000

    Saved recipe: TASKCHUAVAI.compute_Test_Python

--- recipes/compute_Test_Python.py
+++ recipes/compute_Test_Python.py
@@ -1,19 +1,16 @@
 import dataiku
 
-# Read the input dataset
+# Đọc biến partition từ Scenario (lấy từ custom variables)
+current_make = dataiku.get_custom_variables()["current_make"]
+
+# Đọc dataset đầu vào
 mydataset = dataiku.Dataset("norway_new_car_sales_by_make_filtered_2")
 myoutputdataset = dataiku.Dataset("Test_Python")
-current_make = dataiku.get_custom_variables()["current_make"]
+
+# Lọc dữ liệu theo partition (Make) hiện tại
 df = mydataset.get_dataframe()
+partition_df = df[df['Make'] == current_make]
 
-# Lấy danh sách các giá trị duy nhất của cột 'year'
-unique_Maker = df['Make'].unique()
-
-# Lưu dữ liệu theo partition từng năm
+# Ghi dữ liệu ra dataset đầu ra
 with myoutputdataset.get_writer() as writer:
-    for Make in unique_Maker:
-        # Lọc dữ liệu của từng năm
-        partition_df = df[(df['Make'] == Make)]
-        #myoutputdataset.set_write_partition(str(Make))
-        # Không cần gọi set_write_partition, chỉ cần ghi dataframe
-        writer.write_dataframe(partition_df)
+    writer.write_dataframe(partition_df)
