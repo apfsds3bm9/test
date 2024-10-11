1a9a70f71c9433429ffc258c9b3acdcca7afecec
thinhprovc202@gmail.com <thinhprovc202@gmail.com>
2024-10-11 15:50:46 +0000

    Saved recipe: TASKCHUAVAI.compute_Test_Python

--- recipes/compute_Test_Python.py
+++ recipes/compute_Test_Python.py
@@ -23,13 +23,13 @@ myoutputdataset = dataiku.Dataset("Test_Python")
 df = mydataset.get_dataframe()
 
 # Lấy danh sách các giá trị duy nhất của cột 'year'
-unique_years = df['Make'].unique()
+unique_Maker = df['Make'].unique()
 
 # Lưu dữ liệu theo partition từng năm
 with myoutputdataset.get_writer() as writer:
-    for year in unique_years:
+    for Make in unique_Maker:
         # Lọc dữ liệu của từng năm
-        partition_df = df[(df['Year'] >= 2008) & (df['Year'] <= 209)]
-        
+        partition_df = df[(df['Make'] == Make)]
+        myoutputdataset.set_write_partition(str(Make))
         # Không cần gọi set_write_partition, chỉ cần ghi dataframe
         writer.write_dataframe(partition_df)
