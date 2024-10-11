552dcfafeec7746f67052237bd78fcc7e590fc97
thinhprovc202@gmail.com <thinhprovc202@gmail.com>
2024-10-11 17:19:25 +0000

    Saved recipe: TASKCHUAVAI.compute_Test_Python

--- recipes/compute_Test_Python.py
+++ recipes/compute_Test_Python.py
@@ -6,11 +6,11 @@ current_make = dataiku.get_custom_variables()["current_make"]
 # Đọc dataset đầu vào
 mydataset = dataiku.Dataset("norway_new_car_sales_by_make_filtered_2")
 myoutputdataset = dataiku.Dataset("Test_Python")
-print(f"---------------------------------------------------------{current_make}--------------------------------------------------------")
+print(f"--------------------------------------------------------- Name:{current_make}--------------------------------------------------------")
 # Lọc dữ liệu theo partition (Make) hiện tại
 df = mydataset.get_dataframe()
 partition_df = df[df['Make'] == current_make]
-print(f"---------------------------------------------------------{partition_df.shape}--------------------------------------------------------")
+print(f"--------------------------------------------------------- Shape: {partition_df.shape}--------------------------------------------------------")
 
 # Ghi dữ liệu ra dataset đầu ra
 with myoutputdataset.get_writer() as writer:
