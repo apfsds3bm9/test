c4ccb33185eb3ca1d4901dc957ea0698efc9bd67
thinhprovc666@gmail.com <thinhprovc666@gmail.com>
2024-10-14 01:41:58 +0000

    Saved scenario TEST_REMOTE.Test (id: TEST)

--- scenarios/TEST.py
+++ scenarios/TEST.py
@@ -7,7 +7,7 @@ mydataset = dataiku.Dataset("norway_new_car_sales_by_make_filtered_2")
 df = mydataset.get_dataframe()
 #dataiku.default_project_key()
 print(dataiku.default_project_key())
-project_handle = dataiku.api_client().get_project("TEST_REMOTE")
+project_handle = dataiku.api_client().get_project(dataiku.default_project_key())
 vars = project_handle.get_variables()
     
 # Lấy danh sách các giá trị 'Make'
