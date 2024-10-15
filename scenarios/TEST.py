f5048f8b935c034800cee974c058e6c331035d35
thinhprovc202@gmail.com <thinhprovc202@gmail.com>
2024-10-15 02:05:12 +0000

    Saved scenario TASKCHUAVAI.Test (id: TEST)

--- scenarios/TEST.py
+++ scenarios/TEST.py
@@ -28,6 +28,6 @@ for make in unique_makes:
     
     # Chạy step với build_mode là RECURSIVE_FORCED_BUILD cho từng partition
     #scenario.build_dataset("Test_Python", partitions=make, build_mode="RECURSIVE_FORCED_BUILD")
-    scenario.build_dataset("Test_Python_filtered", partitions=make, build_mode="RECURSIVE_FORCED_BUILD")
+    scenario.build_dataset("Test_Python_filtered", partitions=make, build_mode="RECURSIVE_BUILD")
 
     
