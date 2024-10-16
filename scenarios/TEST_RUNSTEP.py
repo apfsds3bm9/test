85807d6d0721ee5a95cac387c336148246a97f15
thinhprovc666@gmail.com <thinhprovc666@gmail.com>
2024-10-16 02:14:51 +0000

    Saved scenario TEST_REMOTE.Test_runstep (id: TEST_RUNSTEP)

--- scenarios/TEST_RUNSTEP.py
+++ scenarios/TEST_RUNSTEP.py
@@ -1,24 +1,33 @@
-# This sample code helps you get started with the custom scenario API.
-#For more details and samples, please see our Documentation
+import dataiku
 from dataiku.scenario import Scenario
+from dataiku.scenario import BuildFlowItemsStepDefHelper
 
+# Lấy dataset chứa thông tin `Make`
+mydataset = dataiku.Dataset("norway_new_car_sales_by_make")
+df = mydataset.get_dataframe()
+#dataiku.default_project_key()
+print(dataiku.default_project_key())
+project_handle = dataiku.api_client().get_project(dataiku.default_project_key())
+vars = project_handle.get_variables()
+    
+# Lấy danh sách các giá trị 'Make'
+unique_makes = df['Make'].unique()
 # The Scenario object is the main handle from which you initiate steps
 scenario = Scenario()
+unique_makes = unique_makes[:6]
+# Building a dataset - Lặp qua danh sách các `Make`
+for make in unique_makes:
+    vars['standard']["makes_list"] = str(make)
+    project_handle.set_variables(vars)
+    # Khởi tạo một step mới cho từng `Make`
+    step = BuildFlowItemsStepDefHelper(f"Data Preparation for {make}")
+    
+    # Add các dataset đầu vào và đầu ra
+    step.add_dataset("norway_new_car_sales_by_make_filtered_2", project_key = "TASKCHUAVAI", partitions=make)  # Thay partition bằng make
+    step.add_dataset("Test_Python", project_key = "TASKCHUAVAI", partitions=make)  # Thêm partition vào dataset đầu ra
+    
+    # Chạy step với build_mode là RECURSIVE_FORCED_BUILD cho từng partition
+    #scenario.build_dataset("Test_Python", partitions=make, build_mode="RECURSIVE_FORCED_BUILD")
+    scenario.build_dataset("Test_Python_filtered", partitions=make, build_mode="RECURSIVE_BUILD")
 
-# A few example steps follow
-
-# Building a dataset
-scenario.build_dataset("customers_prepared", partitions="2015-01-03")
-
-# Controlling the train of a dataset
-train_ret = scenario.train_model("uSEkldfsm")
-trained_model = train_ret.get_trained_model()
-performance = trained_model.get_new_version_metrics().get_performance_values()
-if performance["AUC"] > 0.85:
-    trained_model.activate_new_version()
-
-# Sending custom reports
-sender = scenario.get_message_sender("mail-scenario", "local-mail") # A messaging channel
-sender.set_params(sender="dss@company.com", recipient="data-scientists@company.com")
-
-sender.send(subject="The scenario is doing well", message="All is good")
+    
