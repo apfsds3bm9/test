25511ba6cccfc5fee6ac532e158b86e7f5cca9d0
thinhprovc666@gmail.com <thinhprovc666@gmail.com>
2024-10-16 02:31:43 +0000

    Saved scenario TEST_REMOTE.Test_runstep (id: TEST_RUNSTEP)

--- scenarios/TEST_RUNSTEP.py
+++ scenarios/TEST_RUNSTEP.py
@@ -25,7 +25,7 @@ for make in unique_makes:
     # Add các dataset đầu vào và đầu ra
     #step.add_dataset("norway_new_car_sales_by_make_filtered_2", project_key = "TASKCHUAVAI", partitions=make)  # Thay partition bằng make
     step.add_dataset("Test_Python", project_key = project_key, partitions=make)  # Thêm partition vào dataset đầu ra
-    
+    step.add_dataset("Test_Python_filtered", project_key = project_key, partitions=make)
     # Chạy step với build_mode là RECURSIVE_FORCED_BUILD cho từng partition
     #scenario.build_dataset("Test_Python", partitions=make, build_mode="RECURSIVE_FORCED_BUILD")
     #scenario.build_dataset("Test_Python_filtered", partitions=make, build_mode="RECURSIVE_BUILD")
