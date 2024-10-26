33d8787306d4e38d66ff8567b9a6b5980f9c5224
thinhprovc666@gmail.com <thinhprovc666@gmail.com>
2024-10-26 16:30:26 +0000

    Saved web app Test_Webapp (TEST_REMOTE.BoJoYI5)

--- web_apps/BoJoYI5/backend.py
+++ web_apps/BoJoYI5/backend.py
@@ -45,10 +45,10 @@ def add_json_to_dataset(json):
 
     # This could be a part of data sent by the frontend.
     dataset_name = "mydataset"
-    print("------------------" + json + "------------------")
     client = dataiku.api_client()
     project = client.get_default_project()
     dataset = project.get_dataset(dataset_name)
+    print("------------------" + dataset_name + "------------------")
     if dataset.exists():
         add_content_to_dataset(dataset_name, json)
         return {'status': 200, 'name': json.get('name', '')}
