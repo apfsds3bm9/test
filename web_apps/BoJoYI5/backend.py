62acb00e460d73651c52f1136ac285df27729192
thinhprovc666@gmail.com <thinhprovc666@gmail.com>
2024-10-28 01:47:23 +0000

    Saved web app Test_Webapp (TEST_REMOTE.BoJoYI5)

--- web_apps/BoJoYI5/backend.py
+++ web_apps/BoJoYI5/backend.py
@@ -38,39 +38,3 @@ def first_form():
     return {'status': 200, 'reason': name}
 
 
-def add_content_to_dataset(name, json):
-    """
-    Add a new row in JSON format to an existing data.
-    :param name: Name of the dataset.
-    :param json: Value to append.
-    """
-    dataset = dataiku.Dataset(name)
-    df = dataset.get_dataframe()
-    df = df.append(json, ignore_index=True)
-    logging.info(df.head())
-    dataset.write_dataframe(df)
-
-
-def add_json_to_dataset(json):
-    """
-    Add a row to a dataset, only if the dataset exists.
-    :param json: Value to add.
-    :return: a dict representing the result of the addition.
-    """
-    # This could be a part of data sent by the frontend.
-    dataset_name = "L1"
-    client = dataiku.api_client()
-    project_handle = dataiku.api_client().get_project(dataiku.default_project_key())
-    vars = project_handle.get_variables()
-    PROJECT_KEY = dataiku.default_project_key()
-    vars["standard"]["Test"] = dataset_name
-    project_handle.set_variables(vars)
-    project = client.get_default_project()
-    dataset = project.get_dataset(dataset_name)
-    scenario = DSSScenario(client, PROJECT_KEY, "TEST_RUNSTEP")
-    print("------------------" + dataset_name + "------------------")
-    if dataset.exists():
-        add_content_to_dataset(dataset_name, json)
-        return {'status': 200, 'name': json.get('name', '')}
-    else:
-        return {'status': 400, 'reason': json}, scenario.run()
