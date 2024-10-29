7a799f1ce5448c16f977e7a375411d7ab7e814e4
thinhprovc666@gmail.com <thinhprovc666@gmail.com>
2024-10-29 02:26:19 +0000

    Recipe TEST_REMOTE.compute_Test_sum_and_mul saved through notebook notebook editor for compute_Test_sum_and_mul

--- recipes/compute_Test_sum_and_mul.py
+++ recipes/compute_Test_sum_and_mul.py
@@ -1,3 +1,4 @@
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # -*- coding: utf-8 -*-
 import dataiku
 import pandas as pd, numpy as np
@@ -13,8 +14,16 @@ norway_new_car_sales_by_make_filtered_1_prepared_df = norway_new_car_sales_by_ma
 # NB: DSS also supports other kinds of APIs for reading and writing data. Please see doc.
 
 Test_sum_and_mul_df = norway_new_car_sales_by_make_filtered_1_prepared_df # For this sample code, simply copy input to output
+Test_sum_and_mul_df.Year.dtype()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
+sum_int = int(dataiku.get_custom_variables()["sum"])
+mul_int = int(dataiku.get_custom_variables()["mul"])
+Test_sum_and_mul_df["sum_Year"] = Test_sum_and_mul_df["Year"] + sum_int
+Test_sum_and_mul_df["mul_Year"] = Test_sum_and_mul_df["Year"] * mul_int
+Test_sum_and_mul_df.head()
 
+# -------------------------------------------------------------------------------- NOTEBOOK-CELL: CODE
 # Write recipe outputs
 Test_sum_and_mul = dataiku.Dataset("Test_sum_and_mul")
 Test_sum_and_mul.write_with_schema(Test_sum_and_mul_df)
