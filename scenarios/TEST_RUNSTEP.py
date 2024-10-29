d67bba569f84b33fad30135b43d90cced4f31b0e
thinhprovc666@gmail.com <thinhprovc666@gmail.com>
2024-10-29 02:51:19 +0000

    Saved scenario TEST_REMOTE.Test_runstep (id: TEST_RUNSTEP)

--- scenarios/TEST_RUNSTEP.py
+++ scenarios/TEST_RUNSTEP.py
@@ -15,8 +15,14 @@ unique_makes = df['Make'].unique()
 # The Scenario object is the main handle from which you initiate steps
 scenario = Scenario()
 unique_makes = unique_makes[:6]
+vars['standard']["makes_list"] = str(make)
+project_handle.set_variables(vars)
+step = BuildFlowItemsStepDefHelper(f"Data Preparation for {make}") 
+step.add_dataset("norway_new_car_sales_by_make_filtered_1_prepared", project_key = project_key) 
+step.add_dataset("Test_sum_and_mul", project_key = project_key)
+scenario.run_step(step.get_step())
 # Building a dataset - Lặp qua danh sách các `Make`
-for make in unique_makes:
+'''for make in unique_makes:
     vars['standard']["makes_list"] = str(make)
     project_handle.set_variables(vars)
     # Khởi tạo một step mới cho từng `Make`
@@ -28,6 +34,6 @@ for make in unique_makes:
     #step.add_dataset("Test_Python", project_key = project_key, partitions=make)  # Thêm partition vào dataset đầu ra
     #step.add_dataset("Test_Python_filtered", project_key = project_key, partitions=make)
     # Chạy step với build_mode là RECURSIVE_FORCED_BUILD cho từng partition
-    scenario.run_step(step.get_step())
+    scenario.run_step(step.get_step())'''
 
     
